303e61808b07f8070293f68f838591208a7b9527
Myriam <myriamhueber@gmail.com>
2020-11-17 13:11:02 +0100

    amelioration

--- tag.php
+++ tag.php
@@ -1,1 +1,13 @@
-<?php the_tags('Tags:',',','<br/>'); ?>
+
+
+<?php
+get_header();
+?>
+<div class="category">
+<h1>tag Namme xxxx <?php the_tags('Tags:',',','<br/>'); ?> </h1>
+<?php get_template_part('loop'); ?>
+</div>
+<?php
+get_sidebar();
+get_footer();
+?>
